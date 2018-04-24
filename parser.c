c8bed1fcdb64ec20fcb494586239064580d1d009
xxDOOMbox <shawwian@gmail.com>
2018-04-23 23:17:46 -0400

    destructors

--- parser.c
+++ parser.c
@@ -3,58 +3,84 @@
 #include <editline/readline.h>
 #include "mpc.h"
 
-typedef struct lval {
-  int type;
-  long num;
-  char* err;
-  char* sym;
-  int count; 
-  struct lval** cell; //cell points to a location where we store a list of lval* 
+typedef struct lval 
+{
+    int type;
+    long num;
+    char* err;
+    char* sym;
+    int count; 
+    struct lval** cell; //cell points to a location where we store a list of lval* 
 } lval;
 
 enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR };
 enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 
-static char pct = '%'; 
+static char mod = '%'; 
 
 // construct a pointer to a new lval number
 lval* lval_num (long x) 
 {
-  lval* v = malloc(sizeof(lval));
-  v->type = LVAL_NUM;
-  v->num = x;
-  return v;
+    lval* v = malloc(sizeof(lval));
+    v->type = LVAL_NUM;
+    v->num = x;
+    return v;
 }
 
 // construct a pointer to a new lval error
 lval* lval_err (char* m) 
 {
-  lval* v = malloc(sizeof(lval));
-  v->type = LVAL_ERR;
-  v->err = malloc(strlen(m) + 1);
-  strcpy(v->err, m);
-  return v;
+    lval* v = malloc(sizeof(lval));
+    v->type = LVAL_ERR;
+    v->err = malloc(strlen(m) + 1);
+    strcpy(v->err, m);
+    return v;
 }
 
 // construct a pointer to a new lval symbol
 lval* lval_sym (char* s) 
 {
-  lval* v = malloc(sizeof(lval));
-  v->type = LVAL_SYM;
-  v->sym = malloc(strlen(s) + 1);
-  strcpy(v->sym, s);
-  return v;
+    lval* v = malloc(sizeof(lval));
+    v->type = LVAL_SYM;
+    v->sym = malloc(strlen(s) + 1);
+    strcpy(v->sym, s);
+    return v;
 }
 
 // construct a pointer to a new empty lval s-expression
-lval* lval_sexpr(void) {
-  lval* v = malloc(sizeof(lval));
-  v->type = LVAL_SEXPR;
-  v->count = 0;
-  v->cell = NULL;
-  return v;
+lval* lval_sexpr(void) 
+{
+    lval* v = malloc(sizeof(lval));
+    v->type = LVAL_SEXPR;
+    v->count = 0;
+    v->cell = NULL;
+    return v;
 }
 
+void lval_del(lval* v) 
+{
+    switch (v->type) 
+    {
+        /* Do nothing special for number type */
+        case LVAL_NUM: break;
+
+        /* For Err or Sym free the string data */
+        case LVAL_ERR: free(v->err); break;
+        case LVAL_SYM: free(v->sym); break;
+
+        /* If Sexpr then delete all elements inside */
+        case LVAL_SEXPR:
+        for (int i = 0; i < v->count; i++) {
+            lval_del(v->cell[i]);
+        }
+        /* Also free the memory allocated to contain the pointers */
+        free(v->cell);
+        break;
+    }
+
+    /* Free the memory allocated for the "lval" struct itself */
+    free(v);
+}
 
 int number_of_nodes(mpc_ast_t* t) 
 {
@@ -70,36 +96,81 @@ int number_of_nodes(mpc_ast_t* t)
     return total; 
 }
 
-long op_eval(char* op, long rand1, long rand2) 
+lval eval_op(char* op, lval rand1, lval rand2) 
 {   
-    if (strcmp(op, "+")  == 0) { return rand1 + rand2; }
-    if (strcmp(op, "-")  == 0) { return rand1 - rand2; }
-    if (strcmp(op, "*")  == 0) { return rand1 * rand2; }
-    if (strcmp(op, "/")  == 0) { return rand1 / rand2; }
-    if (strcmp(op, &pct) == 0) { return rand1 % rand2; }    
-    if (strcmp(op, "^")  == 0) { return (long)pow(rand1, rand2); }
 
-    return 0; 
+    if (rand1.type == LVAL_ERR) { return rand1; }
+    if (rand2.type == LVAL_ERR) { return rand2; }
+
+
+    if (strcmp(op, "+")  == 0) { return lval_num(rand1.num + rand2.num); }
+    if (strcmp(op, "-")  == 0) { return lval_num(rand1.num - rand2.num); }
+    if (strcmp(op, "*")  == 0) { return lval_num(rand1.num * rand2.num); }
+    if (strcmp(op, &mod) == 0) { return lval_num(rand1.num % rand2.num); }    
+    if (strcmp(op, "^")  == 0) { return lval_num( (long)pow(rand1.num, rand2.num) ); }
+    if (strcmp(op, "/")  == 0) 
+    {
+        return rand2.num == 0 
+            ? lval_err(LERR_DIV_ZERO) 
+            : lval_num(rand1.num / rand2.num); 
+    }
+
+    return lval_err(LERR_BAD_OP); 
 }
 
-long eval(mpc_ast_t* t) 
+lval eval(mpc_ast_t* t) 
 {
-    if (strstr(t->tag, "number")) 
-        return atoi(t->contents); 
+    if (strstr(t->tag, "number"))
+    {
+        errno = 0; 
+        long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM); 
+    } 
     
     char* op = t->children[1]->contents;
 
-    long result = eval(t->children[2]);
+    lval result = eval(t->children[2]);
 
     int i = 3; 
     while ( strstr(t->children[i]->tag, "expr") )
     {
-        result = op_eval(op, result, eval(t->children[i])); 
+        result = eval_op(op, result, eval(t->children[i])); 
         i++;  
     }
 
     return result; 
 }
+
+void lval_print(lval v) 
+{
+  switch (v.type) 
+  {
+
+    case LVAL_NUM: 
+        printf("%li", v.num); 
+        break;
+
+    case LVAL_ERR: 
+        switch(v.err)
+        {
+        case LERR_DIV_ZERO: 
+            printf("Error: Division By Zero!"); 
+            break;
+            
+        case LERR_BAD_OP: 
+            printf("Error: Invalid Operator!"); 
+            break;
+            
+        case LERR_BAD_NUM: 
+            printf("Error: Invalid Number!"); 
+            break;
+        }
+    break;
+  }
+}
+
+/* Print an "lval" followed by a newline */
+void lval_println(lval v) { lval_print(v); putchar('\n'); }
 
 int main(int argc, char** argv) 
 {   
@@ -138,8 +209,8 @@ int main(int argc, char** argv)
             // int nodes_num = number_of_nodes(result.output); 
             // printf("Number of nodes: %i\n", nodes_num); 
 
-            long eval_result = eval(result.output);
-            printf("%li\n", eval_result);
+            lval eval_result = eval(result.output);
+            lval_println(eval_result);
             mpc_ast_delete(result.output);
         } 
         else 
@@ -151,7 +222,7 @@ int main(int argc, char** argv)
         free(input); 
     }   
 
-    mpc_cleanup(4, Number, Symbol, Sexpr, Expr, Lip);
+    mpc_cleanup(5, Number, Symbol, Sexpr, Expr, Lip);
     return 0; 
 }
 
